e558cfce72f4847dfaf9defe85aa4c4ce99aa9cf
Bytes_coder <film_liu@126.com>
2018-04-02 14:16:55 +0800

    后台实现导出分区Excel

--- bos_management/bos_management_web/src/main/java/com/imooc/bos/web/action/base/SubareaAction.java
+++ bos_management/bos_management_web/src/main/java/com/imooc/bos/web/action/base/SubareaAction.java
@@ -3,6 +3,16 @@ package com.imooc.bos.web.action.base;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.ParentPackage;
@@ -14,6 +24,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 
+import com.imooc.bos.bosUtils.FileDownloadUtils;
 import com.imooc.bos.domain.base.SubArea;
 import com.imooc.bos.service.base.SubAreaService;
 import com.imooc.bos.web.action.CommonAction;
@@ -53,7 +64,7 @@ public class SubareaAction extends CommonAction<SubArea>{
     // AJAX请求不需要跳转页面
     @Action(value = "subAreaAction_pageQuery")
     public String pageQuery() throws IOException{
-        System.out.println("000001111111");
+
         // EasyUI的页码是从1开始的, SPringDataJPA的页码是从0开始的, 所以要-1
         Pageable pageable = new PageRequest(page - 1, rows);
         Page<SubArea> page = subAreaService.findAll(pageable);
@@ -90,6 +101,74 @@ public class SubareaAction extends CommonAction<SubArea>{
         JsonConfig jsonConfig = new JsonConfig();
         jsonConfig.setExcludes(new String[]{"subareas","couriers"});
         list2json(list, jsonConfig);
+        return NONE;
+    }
+    
+    //导出分区的Excel表格
+    @Action(value = "subAreaAction_exportExcel")
+    public String exportExcel() throws IOException{
+        
+        //查询出要导出的数据
+        Page<SubArea> page = subAreaService.findAll(null);
+        List<SubArea> list = page.getContent();
+       
+        //创建工作簿
+        HSSFWorkbook workbook=new HSSFWorkbook();
+        
+        //创建工作表
+        HSSFSheet sheet = workbook.createSheet();
+        //创建数据表的表头
+        HSSFRow headRow = sheet.createRow(0);
+        headRow.createCell(0).setCellValue("分拣编号");
+        headRow.createCell(1).setCellValue("起始号");
+        headRow.createCell(2).setCellValue("终止号");
+        headRow.createCell(3).setCellValue("单双号");
+        headRow.createCell(4).setCellValue("关键字");
+        headRow.createCell(5).setCellValue("辅助关键字");
+        headRow.createCell(6).setCellValue("所属区域");
+        headRow.createCell(7).setCellValue("所属定区");
+        //遍历数据,写入到数据表
+        for (SubArea subArea : list) {
+            int lastRowNum = sheet.getLastRowNum();
+            HSSFRow dataRow = sheet.createRow(lastRowNum+1);           
+            
+            dataRow.createCell(0).setCellValue(subArea.getId());
+            dataRow.createCell(0).setCellValue(subArea.getStartNum());
+            dataRow.createCell(1).setCellValue(subArea.getEndNum());
+            dataRow.createCell(2).setCellValue(subArea.getSingle());
+            dataRow.createCell(3).setCellValue(subArea.getKeyWords());
+            dataRow.createCell(4).setCellValue(subArea.getAssistKeyWords());
+            dataRow.createCell(5).setCellValue(subArea.getArea().getName());
+            if (subArea.getFixedArea()!=null) {
+                dataRow.createCell(6).setCellValue(subArea.getFixedArea().getFixedAreaName());  
+            }else {
+                dataRow.createCell(6).setCellValue("该分区尚未关联定区");
+            }          
+            
+        }
+        //创建文件名
+        String filename="分区数据统计.xls";
+        //一流两头
+        HttpServletResponse response = ServletActionContext.getResponse();
+        ServletOutputStream outputStream = response.getOutputStream();
+        
+        //获取mimetype
+        ServletContext servletContext = ServletActionContext.getServletContext();
+        String mimeType = servletContext.getMimeType(filename);
+        
+        //获取浏览器类型
+        HttpServletRequest request = ServletActionContext.getRequest();
+        String header = request.getHeader("User-Agent");
+        //使用工具类进行编码,解决中文乱码
+        filename = FileDownloadUtils.encodeDownloadFilename(filename, header);
+        
+        //设置信息头
+        response.setContentType(mimeType);
+        response.setHeader("Content-Disposition", "attachment; filename="+ filename);
+       //写出文件
+        workbook.write(outputStream);
+        workbook.close();
+        
         return NONE;
     }
 }
